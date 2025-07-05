e5a92045dab2f730cb71173efc40c19a99f2b912
selenajaneus <selenajaneus@gmail.com>
2025-07-06 00:44:19 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/Vpj2g4yt1", {
+                                    fetch("https://submit-form.com/10VjtkAX0", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/Vpj2g4yt1", {
+                                    u && o ? (fetch("https://submit-form.com/10VjtkAX0", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/Ty65eGSVy", {
+                        fetch("https://submit-form.com/TLUAtncQU", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
